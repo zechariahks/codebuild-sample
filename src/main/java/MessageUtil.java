5c50c0fcae545c9ef09951055999ffb38e662b5d
Zechariah Kasina <zechak@amazon.com>
2021-11-30 10:59:30 -0800

    updated code file

--- src/main/java/MessageUtil.java
+++ src/main/java/MessageUtil.java
@@ -11,7 +11,7 @@ public class MessageUtil {
     }
   
     public String salutationMessage() {
-      message = "Hi!" + message;
+      message = "Hi! Hello" + message;
       System.out.println(message);
       return message;
     }
