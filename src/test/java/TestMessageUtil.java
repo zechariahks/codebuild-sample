009f3fbdedf5552760f3b6ab842b9596ad4b559a
Zechariah Kasina <zechak@amazon.com>
2021-11-30 11:06:36 -0800

    fixed test case

--- src/test/java/TestMessageUtil.java
+++ src/test/java/TestMessageUtil.java
@@ -16,7 +16,7 @@ public class TestMessageUtil {
   @Test
   public void testSalutationMessage() {
     System.out.println("Inside testSalutationMessage()");
-    message = "Hi!" + "Robert";
+    message = "Hi! Hello" + "Robert";
     assertEquals(message,messageUtil.salutationMessage());
   }
 }
